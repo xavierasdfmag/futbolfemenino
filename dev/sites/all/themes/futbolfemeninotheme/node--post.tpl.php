2e81560dff3149356b20ff858dd75c26d0513acb
Freddy H <freddy01288@gmail.com>
2015-02-10 11:18:48 -0500

    agregando js y probando print $title

--- dev/sites/all/themes/futbolfemeninotheme/node--post.tpl.php
+++ dev/sites/all/themes/futbolfemeninotheme/node--post.tpl.php
@@ -80,217 +80,40 @@
  * @ingroup themeable
  */
 ?>
+
   
-<body>
-  <div class="row show-for-large-up">
-    <figure class="portada">
-      <img src="../img/portada.jpg">
-      <figcaption>
-        <div class="row">
-          <div class="medium-4 columns portada">
-            <p class="tag">BALON DE ORO</p>
-            <p class="title">Las jugadas de Nadine Kessler, la mejor del mundo</p>
-            
-          </div>
-          <div class="medium-3 columns right tentativas">
-            <div class="row">
-              <div class="small-12 columns tentativa">
-                <p class="tag">CR7 MEMES</p>
-                <p class="title">Añade la sabiduría Ronaldo a tus chats</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="small-12 columns tentativa">
-                <p class="tag">CR7 EL CUERPO</p>
-                <p class="title">El atleta más perfecto del siglo veintiuno (por ahora)</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="small-12 columns tentativa">
-                <p class="tag">NEUER ARQUERO INMORTAL</p>
-                <p class="title">Neuer y la tradición de los invencibles arqueros alemanes</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </figcaption>
-    </figure>
-                    
-  </div>
-  <div class="row slider-portada show-for-large-up">
-    <div class="small-12 columns padding-20">
-
-      <ul class="example-orbit-content" data-orbit>
-        <li data-orbit-slide="headline-1">
-          <div class="row">
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-1.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">LIVERPOOL VS MANCHESTER</p>
-                  <p class="titulo">El partido de despedida del gran capitan Gerrard</p>
-                </div>
-              </div>
-            </div>
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-2.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">PROMOVIDA POR NIKE</p>
-                  <p class="titulo">Compra tu zapatillas y llévate tus medias a juego</p>
-                </div>
-              </div>
-            </div>
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-3.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">NOCHE CREMA</p>
-                  <p class="titulo">Una fiesta para toda la familia hincha de universitario</p>
-                </div>
-              </div>
-            </div>                    
-          </div>
-        </li>
-        <li data-orbit-slide="headline-2">
-          <div class="row">
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-1.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">LIVERPOOL VS MANCHESTER</p>
-                  <p class="titulo">El partido de despedida del gran capitan Gerrard</p>
-                </div>
-              </div>
-            </div>
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-2.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">PROMOVIDA POR NIKE</p>
-                  <p class="titulo">Compra tu zapatillas y llévate tus medias a juego</p>
-                </div>
-              </div>
-            </div>
-            <div class="small-4 columns">
-              <div class="row">
-                <div class="small-4 columns imagen">
-                  <img src="../img/not-small-3.png">
-                </div>
-                <div class="small-8 columns noticia">
-                  <p class="llamada">NOCHE CREMA</p>
-                  <p class="titulo">Una fiesta para toda la familia hincha de universitario</p>
-                </div>
-              </div>
-            </div>                    
-          </div>
-        </li>
-      </ul>  
+  <div class="row">
+    <div class="small-12 columns sec-left single-cabecera">
+      <p class="single-tag">SUB 20</p>
+      <p class="single-title"><?php print $title; ?></p>
+      <p class="single-llamada">Entrenan tanto que sus papas piensan que están de compras. Inefables.</p>
     </div>
   </div>
 
-  <div class="row contenedor">
-    <!-- PARTE IZQUIERDA -->
-    <div class="small-12 medium-12 large-8 columns sec-left">
-      <!-- SECCION DE NOTICIAS PORTADA TIPO UNO -->
-      <div class="row seccion-type-one hide-for-large-up">
-        <div class="small-12 columns">
-          <p class="title-section">BALON DE ORO</p>
-        </div>
-        <div class="small-12 medium-8 columns">
-          <h3 class="title-news">Las jugadas de Nadine Kessler, la mejor del mundo</h3>
-          <p class="bajada">Entrenan tanto que sus papas piensan que están de compras. Inefables.</p>
-          <img src="../img/portada.jpg">
-          
-        </div>
-        <div class="small-12 medium-4 columns">
-          <ul>
-            <li><a href="#">Añade la sabiduría Ronaldo a tus chats</a></li>
-            <li><a href="#">El atleta más perfecto del siglo veintiuno (por ahora)</a></li>
-            <li><a href="#">Neuer y la tradición de los invencibles arqueros alemanes</a></li>
-          </ul>
-        </div>
-      </div>    
-      <!-- SECCION DE NOTICIAS TIPO UNO -->
-      <div class="row seccion-type-one">
-        <div class="small-12 columns">
-          <p class="title-section">SUB 20</p>
-        </div>
-        <div class="small-12 medium-8 columns">
-          <h3 class="title-news">Las señoritas de la sub 20 entrenan sin descanso en la videna</h3>
-          <p class="bajada">Entrenan tanto que sus papas piensan que están de compras. Inefables.</p>
-          <img src="../img/not-type-one.png">
-          
-        </div>
-        <div class="small-12 medium-4 columns">
-          <ul>
-            <li><a href="#">Pepinos encurtidos para combatir calambre, aprende a prepararlo</a></li>
-            <li><a href="#">Una guía de licras para evitar estiramiento inútiles</a></li>
-            <li><a href="#">Qué hacen las profesionales para mantenerse en forma antes de un partido</a></li>
-          </ul>
-        </div>
-      </div>
-      <!-- SECCION DE NOTICIAS TIPO DOS -->
-      <div class="row seccion-type-two">
-        <div class="small-12 columns">
-          <p class="title-section">PROMOVIDA POR NIKE</p>
-        </div>      
-        <div class="small-12 columns">
-          <img src="../img/not-type-two.png">
-        </div>
-        <div class="small-12 medium-7 columns">
-          <h3 class="title-news">Nike celebra a las mujeres deportivas con una nueva línea de pantalonetas</h3>
-        </div>
-        <div class="small-12 medium-5 columns">
-          <p class="bajada">Es una edición limitada exclusiva para Perú, Chile y Bolivia.</p>
-        </div>
-      </div>
-      <!-- SECCION DE NOTICIAS TIPO TRES -->
-      <div class="row seccion-type-three">
-        <div class="small-12 columns">
-          <p class="title-section">MUNDIAL CANADÁ 2015</p>
-        </div>        
-        <div class="small-8 columns">
-          <img src="../img/not-type-three.jpg">  
-          <div class="row">
-            <div class="small-6 columns">
-              <ul>
-                <li>
-                  <a href="#">Un mundial sin importar el género no puede jugarse en canchas sintéticas</a>
-                </li>
-              </ul>
-            </div>
-            <div class="small-6 columns">
-              <ul>
-                <li>
-                  <a href="#">Las selecciones a las que debemos mirar para Canadá 2015.</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+  <div class="row">
 
+
+    <div class="small-12 large-8 columns sec-left">
+      <div class="row">
+        <div class="small-12 columns single-img-portada">
+          <img src="../img/not-type-one.png"> 
+        </div>
+        <div class="small-12 columns single-contenido">
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium, justo et volutpat ultricies, ante elit fringilla orci, at aliquet neque diam et dolor. Nullam suscipit blandit varius. Phasellus euismod aliquam odio. Nunc fermentum magna at varius sollicitudin. In hac habitasse platea dictumst. Nam vestibulum lacus quis nibh faucibus ullamcorper. Proin sit amet dignissim augue, ac vestibulum neque. Pellentesque egestas urna vel turpis scelerisque, ac fermentum mauris iaculis. Proin lorem arcu, hendrerit id tristique faucibus, eleifend nec nunc. Fusce scelerisque sed quam placerat efficitur. Morbi sed nunc nibh. Praesent fermentum massa sed sagittis aliquet. In imperdiet lorem erat, rhoncus semper sapien porttitor ut. Phasellus pulvinar vel magna vel malesuada.</p>
+
+          <p>In mollis egestas aliquam. In vitae posuere massa. Donec tempus mauris ligula, in commodo quam gravida eget. Praesent sit amet varius dui, aliquam vestibulum dui. Praesent vitae elementum est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus nisl, maximus et elit ut, facilisis tincidunt lectus. Sed sit amet vestibulum massa. Pellentesque bibendum sagittis nisl, nec gravida nunc volutpat et. Aliquam erat volutpat. Aenean eget libero at orci pellentesque dictum. Nunc scelerisque lectus a commodo euismod. Sed arcu dui, elementum at turpis ut, scelerisque sagittis enim.</p>
+
+          <p>Quisque mi odio, viverra sed malesuada euismod, feugiat id leo. Nullam lacus felis, lobortis ut luctus varius, sodales vitae magna. Ut dui orci, congue eu massa vel, blandit eleifend sapien. Curabitur egestas ut arcu eget tincidunt. Cras tincidunt lectus id massa venenatis fringilla eget a nisl. Mauris iaculis et libero sit amet dictum. Vestibulum nunc sem, auctor rhoncus sagittis nec, aliquet vel enim. Praesent quam lectus, mollis non est ut, maximus tincidunt mi. Maecenas at elementum velit. Quisque ullamcorper nulla in magna accumsan lacinia. Quisque iaculis scelerisque erat. Nulla nisl metus, condimentum blandit elit at, scelerisque consectetur nunc.</p>
+
+          <img src="../img/not-type-one.png">       
           
         </div>
-        <div class="small-4 columns">
-          <h3 class="title-news">Todo lo que necesitas saber del mundial de Canadá 2015 en esta infografía</h3>
-          <p class="bajada">Desde los estadios hasta los equipos representantes.</p>
-    
-        </div>
-      </div>      
 
+
+      </div>
 
     </div>
-
 
     <div class="small-12 medium-12 large-4 columns">
       <div class="row show-for-large-up">
@@ -318,62 +141,52 @@
         </div>
       </div>
 
-      <div class="show-for-medium-only text-center">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- BlunBlun_728x90 -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:728px;height:90px"
-             data-ad-client="ca-pub-3000094281853126"
-             data-ad-slot="9376773571"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>        
+      <div class=" row show-for-medium-only text-center">
+        <div class="small-12 columns">
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <!-- BlunBlun_728x90 -->
+          <ins class="adsbygoogle"
+               style="display:inline-block;width:728px;height:90px"
+               data-ad-client="ca-pub-3000094281853126"
+               data-ad-slot="9376773571"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>   
+        </div>     
       </div>
+
+      <div class="row noticia-relacionada">
+        <div class="small-6 medium-4 large-12 columns">
+          <figure>
+              <img src="../img/not-sponsor-1.png">
+            <figcaption>
+            </figcaption>
+          </figure>       
+          <p class="llamada">FREE STYLE</p>
+          <p class="title"> Esta mujer puede hacer más de quinientas pataditas en un sólo intento</p>
+        </div>
+        <div class="small-6 medium-4 large-12 columns">
+          <figure>
+              <img src="../img/not-sponsor-2.png">
+            <figcaption>
+            </figcaption>
+          </figure>       
+          <p class="llamada">GOLES SON AMORES</p>
+          <p class="title">Viste el gol más rápido del fútbol femenino? Son asombrosos</p>          
+        </div>
+        <div class="small-6 medium-4 large-12 columns">
+          <figure>
+              <img src="../img/not-sponsor-2.png">
+            <figcaption>
+            </figcaption>
+          </figure>
+          <p class="llamada">GOLES SON AMORES</p>
+          <p class="title">Viste el gol más rápido del fútbol femenino? Son asombrosos</p>          
+        </div>        
+      </div>
+
+
     </div>
-
-
-  </div>
-
-  <div class="row">
-    <div class="small-12 columns sponsor-banner">
-      <img src="../img/banner-sponsor.jpg">
-    </div>
-  </div>
-
-  <div class="row sponsor-news">
-    <div class="small-6 medium-4 columns">
-      <figure>
-          <img src="../img/not-sponsor-1.png">
-        <figcaption>
-        </figcaption>
-      </figure>
-      <p class="llamada">FREE STYLE</p>
-      <p class="titulo">Esta mujer puede hacer más de quinientas pataditas en un sólo intento</p>
-    </div>
-    <div class="small-6 medium-4 columns">
-      <figure>
-          <img src="../img/not-sponsor-2.png">
-        <figcaption>
-        </figcaption>
-      </figure>
-      <p class="llamada">GOLES SON AMORES</p>
-      <p class="titulo">Viste el gol más rápido del fútbol femenino? Son asombrosos</p>
-    </div>      
-    <div class="small-6 medium-4 columns">
-      <figure>
-          <img src="../img/not-sponsor-2.png">
-        <figcaption>
-        </figcaption>
-      </figure>
-      <p class="llamada">GOLES SON AMORES</p>
-      <p class="titulo">Viste el gol más rápido del fútbol femenino? Son asombrosos</p>
-    </div>    
     
+
   </div>
-
-
-
-</body>
-</html>
-
-<?php include 'includes/footer.php';?>
