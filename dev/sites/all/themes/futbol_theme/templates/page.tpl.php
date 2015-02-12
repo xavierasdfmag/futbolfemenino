0884976c029cf4de198aaf230d9733c3d5d61717
Freddy H <freddy01288@gmail.com>
2015-02-12 15:23:33 -0500

    agregando js en footer

--- dev/sites/all/themes/futbol_theme/templates/page.tpl.php
+++ dev/sites/all/themes/futbol_theme/templates/page.tpl.php
@@ -1,1 +1,5 @@
 <?php print render($page['content']) ?>
+
+<?php drupal_add_js($theme_path.'/js/vendor/jquery.js', array('type' => 'file', 'scope' => 'footer')); ?>
+<?php drupal_add_js($theme_path.'/js/foundation.min.js', array('type' => 'file', 'scope' => 'footer')); ?>
+<?php drupal_add_js($theme_path.'/js/app.js', array('type' => 'file', 'scope' => 'footer')); ?>
