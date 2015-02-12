11838ebf09e37fdb77dbf1e34a7d9909366cc2db
carlos calderon <cadermino@gmail.com>
2015-02-12 15:35:14 -0500

    javascript footer

--- dev/sites/all/themes/futbol_theme/template.php
+++ dev/sites/all/themes/futbol_theme/template.php
@@ -1,31 +1,29 @@
 <?php
 
-/**
- * Implementation of theme_panels_default_style_render_region().
- */
-function precision_panels_default_style_render_region($vars) {
-  return implode($vars['panes']);
-}
+$viewport = array(
+  '#tag' => 'meta', 
+  '#attributes' => array(
+    'name' => 'viewport', 
+    'content' => 'width=device-width, initial-scale=1.0, user-scalable=no',
+  ),
+);
+drupal_add_html_head($viewport, 'viewport');
 
-/**
- * Generic function that modifies some variables in all Precision layouts.
- */
-function precision_check_layout_variables(&$vars) {
-  $vars['css_id'] = strtr($vars['css_id'], '_', '-');
-}
-
-/**
- * Implementation of template_process_html().
- */
-function precision_process_html(&$variables, $hook) {
-  // Check if overlay is activated.
-  if (theme_get_setting('precision_show_overlay')) {
-    // Extract and put back the body classes.
-    $variables['classes_array'][] = 'show-grid';
-    $variables['classes'] = implode(' ', $variables['classes_array']);
-    // Add the overlay css to the page.
-    $styles = (drupal_add_css(drupal_get_path('theme', 'precision') . '/debug/debug.css'));
-    $variables['css'] = $styles;
-    $variables['styles'] = drupal_get_css($variables['css']);
-  }
-}
+drupal_add_js(path_to_theme() . '/js/vendor/jquery.js', array(
+  'scope' => 'footer',
+  'group' => JS_THEME,
+  'every_page' => TRUE,
+  'weight' => 1,
+));
+drupal_add_js(path_to_theme() . '/js/foundation.min.js', array(
+  'scope' => 'footer',
+  'group' => JS_THEME,
+  'every_page' => TRUE,
+  'weight' => 2,
+));
+drupal_add_js(path_to_theme() . '/js/app.js', array(
+  'scope' => 'footer',
+  'group' => JS_THEME,
+  'every_page' => TRUE,
+  'weight' => 3,
+));
