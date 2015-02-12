4f3c22b4469001f12518af3511c6c85a642cc7df
Freddy H <freddy01288@gmail.com>
2015-02-12 18:29:25 -0500

    subiendo template de views news sponsored

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--slider-portada-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--slider-portada-home--block.tpl.php
@@ -2,12 +2,19 @@
   <ul class="example-orbit-content" data-orbit>
     <li data-orbit-slide="headline-1">
       <div class="row">
-
 			  <?php foreach ($rows as $id => $row): ?>
 			    <?php print $row;?>
 				<?php endforeach; ?>
-
       </div>
-    </li>          
+    </li>
+    <li data-orbit-slide="headline-2">
+      <div class="row">
+				<?php
+					for ($x=0;$x<4; $x++)
+						print $row[$x];
+				?>
+      </div>
+    </li>
+
   </ul>  
 </div>
