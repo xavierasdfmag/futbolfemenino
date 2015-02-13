9b54dd6fae7d46d11cff60e6c3b91a54ed6a7b1c
Freddy H <freddy01288@gmail.com>
2015-02-13 16:09:37 -0500

    ...

--- dev/sites/all/themes/futbol_theme/views-view-fields--portada-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-fields--portada-home--block.tpl.php
@@ -11,12 +11,49 @@
 	$body = $fields['body']->content;
 	$image = $fields['field_imagen']->content;
 ?>
-<img src="<?php print $image; ?>">
-<figcaption>
-  <div class="row">
-    <div class="medium-4 columns portada">
-      <p class="tag"><?php print $id; ?></p>
-      <p class="title"><?php print $title; ?></p>
-    </div>
-  </div>
-</figcaption>
+
+<?php 
+  switch ($id) {
+    case 1: ?>
+      <img src="<?php print $image; ?>">
+      <figcaption>
+        <div class="row">
+          <div class="medium-4 columns portada">
+            <p class="tag"><?php print $id; ?></p>
+            <p class="title"><?php print $title; ?></p>
+          </div>
+          <div class="medium-3 columns right tentativas">
+      <?php
+      break;
+
+    case 4: ?>
+            <div class="row">
+              <div class="small-12 columns tentativa">
+                <p class="tag"><?php print $id; ?></p>
+                <p class="title"><?php print $title; ?></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </figcaption>
+      <?php
+      break;
+    
+    default: ?>
+        <div class="row">
+          <div class="small-12 columns tentativa">
+            <p class="tag"><?php print $id; ?></p>
+            <p class="title"><?php print $title; ?></p>
+          </div>
+        </div>
+      <?php
+      break;
+  }
+?>
+
+
+
+
+
+
+
