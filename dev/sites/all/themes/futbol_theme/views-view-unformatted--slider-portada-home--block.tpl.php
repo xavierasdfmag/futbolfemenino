5efc284feab9c09f69a037df418f127eb2c415a9
Freddy H <freddy01288@gmail.com>
2015-02-13 15:28:20 -0500

    ...

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--slider-portada-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--slider-portada-home--block.tpl.php
@@ -1,7 +1,7 @@
 <div class="small-12 columns padding-20">
   <ul class="example-orbit-content" data-orbit>
 			<?php
-        foreach ($rows as $key => $row) {
+        foreach ($rows as $id => $row) {
 					print $row;
         }
 			?>
