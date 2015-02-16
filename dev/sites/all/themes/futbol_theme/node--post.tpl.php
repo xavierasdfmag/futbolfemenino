e1572071d807e4e5dd480dd0242892794c517f8a
Freddy H <freddy01288@gmail.com>
2015-02-16 14:45:54 -0500

    agregando node--post.tpl.php

--- dev/sites/all/themes/futbol_theme/node--post.tpl.php
+++ dev/sites/all/themes/futbol_theme/node--post.tpl.php
@@ -121,7 +121,7 @@
           <!-- <img src="../img/not-type-one.png">  -->
         </div>
         <div class="small-12 columns single-contenido">
-          <?php print $content['body'][0]; ?>
+          <?php print $node->body['und']['0']['value'] ?>
         </div>
 
 
