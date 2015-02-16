1cd42569e4893be741a352fbaa6a7f04282a7a4a
Freddy H <freddy01288@gmail.com>
2015-02-16 12:21:35 -0500

    agregando node--post.tpl.php

---
+++ dev/sites/all/themes/futbol_theme/plugins/layouts/futboltheme_interna_template/futboltheme-interna-template.tpl.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * This layout is designed to be the site template layout when using
+ * the Panels Everywhere module.
+ */
+?>
+<div class="row publicidad-header show-for-medium-up">
+  <div class="medium-12 medium-centered columns text-center">
+    <?php print render($content['publicidad-header']); ?>
+  </div>
+</div>
+<div class="row encabezado-header">
+  <?php print render($content['encabezado-header']); ?>
+</div>
+<div class="row main-menu">
+  <?php print render($content['main-menu']); ?>
+</div>
+<div class="row contenedor">
+
+      <?php print render($content['left-side']); ?>
+
+      <?php print render($content['right-side']); ?>
+
+</div>
+<div class="row banner-sponsor">
+  <?php print render($content['banner-sponsor']); ?>
+</div>
+<div class="row sponsor-news">
+  <?php print render($content['sponsor-news']); ?>
+</div>
