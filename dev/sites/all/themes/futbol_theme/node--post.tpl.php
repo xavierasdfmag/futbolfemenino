93e3d1349648dceab53e4c5136ad920bf3e0e57f
Freddy H <freddy01288@gmail.com>
2015-02-16 12:43:05 -0500

    agregando node--post.tpl.php

--- dev/sites/all/themes/futbol_theme/node--post.tpl.php
+++ dev/sites/all/themes/futbol_theme/node--post.tpl.php
@@ -81,7 +81,10 @@
  */
 ?>
 
-  <?php //print_r($node); ?>  
+  <?php //print_r($node); 
+    kpr($fields);
+    dpm($fields);
+  ?>  
   <div class="row">
     <div class="small-12 columns sec-left single-cabecera">
       <p class="single-tag">
