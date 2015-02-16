6bc1823f48c6fc779acbe6788a1ef9fd57087093
Freddy H <freddy01288@gmail.com>
2015-02-16 18:32:36 -0500

    agregando node--post.tpl.php

--- dev/sites/all/themes/futbol_theme/views-view-fields--right-interna---block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-fields--right-interna---block.tpl.php
@@ -5,7 +5,7 @@
   $path = $base_url.'/'.$theme_path;
   $pathfile = variable_get('file_public_path', conf_path() . '/files/'); 
 
-	kpr($fields);
+	// kpr($fields);
 	// dpm($fields);
 	$title = $fields['title']->content;
 	$tag = $fields['field_tag']->content;
