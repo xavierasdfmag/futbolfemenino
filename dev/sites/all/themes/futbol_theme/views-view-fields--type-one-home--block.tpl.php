6c2db017767f3d465a292c570926cbf905897ce7
Freddy H <freddy01288@gmail.com>
2015-02-23 17:29:59 -0500

    type one home

--- dev/sites/all/themes/futbol_theme/views-view-fields--type-one-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-fields--type-one-home--block.tpl.php
@@ -9,7 +9,29 @@
 	// dpm($fields);
   $title = $fields['title']->content;
   $tag = $fields['field_tag']->content;
-  $texto_corto = $fields['field_texto_corto']->content;
+  $llamada = $fields['field_llamada']->content;
   $image = $fields['field_imagen']->content;
 ?>
-<?php print $title; ?>
+<?php  $total = count($view->result); ?>
+
+  <?php if ($count == 1) : ?>
+    <div class="small-12 columns">
+      <p class="title-section"><?php print $tag; ?></p>
+    </div>
+    <div class="small-12 medium-8 columns">
+      <h3 class="title-news"><?php print $title; ?></h3>
+      <p class="bajada"><?php print $llamada; ?></p>
+      <img src="<?php print $image; ?>">
+      
+    </div>
+    <div class="small-12 medium-4 columns">
+      <ul>
+  <?php elseif($count > 1 && $count <= $total) : ?>
+        <li><a href="#"><?php print $title; ?></a></li>
+  <?php elseif($count == $total) : ?>
+      </ul>
+    </div>
+
+
+  <?php endif ?>
+  <?php $count++; ?>
