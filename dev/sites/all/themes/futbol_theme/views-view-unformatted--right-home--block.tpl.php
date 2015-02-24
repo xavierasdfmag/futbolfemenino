a2c0a456d2494956ee73aa15c97a90155b04a85f
Freddy H <freddy01288@gmail.com>
2015-02-24 11:54:38 -0500

    type three home

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--right-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--right-home--block.tpl.php
@@ -1,44 +1,40 @@
-    <div class="small-12 medium-12 large-4 columns">
-      <div class="row show-for-large-up">
-        <div class="large-12 columns">
-          <div class="row suscribir-large">
-            <div class="large-12 columns ">
-              <h4>Únete a nuestra tribuna</h4>
-              <p>Recibe lo mejor de futbolfemenino.pe en tu correo</p>            
-            </div>
-            <div class="large-7 columns padding-right-cero"><input type="text"></input></div>
-            <div class="large-5 columns padding-left-cero"><a href="#" class="button small">Suscribir</a></div>
-          </div>
+  <div class="row show-for-large-up">
+    <div class="large-12 columns">
+      <div class="row suscribir-large">
+        <div class="large-12 columns ">
+          <h4>Únete a nuestra tribuna</h4>
+          <p>Recibe lo mejor de futbolfemenino.pe en tu correo</p>            
         </div>
-
-        <div class="large-12 columns text-right">
-          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <!-- BlunBlun_300x250 -->
-          <ins class="adsbygoogle"
-               style="display:inline-block;width:300px;height:600px"
-               data-ad-client="ca-pub-3000094281853126"
-               data-ad-slot="7900040372"></ins>
-          <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
-        </div>
+        <div class="large-7 columns padding-right-cero"><input type="text"></input></div>
+        <div class="large-5 columns padding-left-cero"><a href="#" class="button small">Suscribir</a></div>
       </div>
-
-      <div class=" row show-for-medium-only text-center">
-        <div class="small-12 columns">
-          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <!-- BlunBlun_728x90 -->
-          <ins class="adsbygoogle"
-               style="display:inline-block;width:728px;height:90px"
-               data-ad-client="ca-pub-3000094281853126"
-               data-ad-slot="9376773571"></ins>
-          <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>   
-        </div>     
-      </div>
-
     </div>
 
+    <div class="large-12 columns text-right">
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- BlunBlun_300x250 -->
+      <ins class="adsbygoogle"
+           style="display:inline-block;width:300px;height:600px"
+           data-ad-client="ca-pub-3000094281853126"
+           data-ad-slot="7900040372"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div>
+    </div>
 
-  </div>    
+    <div class=" row show-for-medium-only text-center">
+      <div class="small-12 columns">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- BlunBlun_728x90 -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-3000094281853126"
+             data-ad-slot="9376773571"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>   
+      </div>     
+    </div>
+
+  </div>
