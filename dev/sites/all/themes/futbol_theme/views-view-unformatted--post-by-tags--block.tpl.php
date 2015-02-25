27a11f8246d0036b18f05456074c3e66573aa715
Freddy H <freddy01288@gmail.com>
2015-02-25 15:58:05 -0500

    agregando view by tags

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--post-by-tags--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--post-by-tags--block.tpl.php
@@ -1,3 +1,6 @@
-<?php foreach ($rows as $id => $row): ?>
-  <?php print $row;?>
-<?php endforeach; ?>
+<div class="row">
+  <div class="small-12 large-8 columns sec-left">
+		<?php foreach ($rows as $id => $row): ?>
+		  <?php print $row;?>
+		<?php endforeach; ?>
+	</div>
