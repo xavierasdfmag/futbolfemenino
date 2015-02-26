e7f5004f945aab26bbe55e3a2b607b0995d75af0
Freddy H <freddy01288@gmail.com>
2015-02-26 10:22:27 -0500

    agregando vista banner sponsor

---
+++ dev/sites/all/themes/futbol_theme/views-view-fields--banner-sponsor--block.tpl.php
@@ -0,0 +1,13 @@
+<?php 
+  global $language;
+  global $base_url;
+  global $theme_path;
+  $path = $base_url.'/'.$theme_path;
+  $pathfile = variable_get('file_public_path', conf_path() . '/files/'); 
+
+	// kpr($fields);
+	// dpm($fields);
+	$title = $fields['title']->content;
+	$image = $fields['field_imagen']->content;
+?>
+<img src="<?php print $image; ?>">
