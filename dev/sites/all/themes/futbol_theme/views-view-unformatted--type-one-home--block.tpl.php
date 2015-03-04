4b1d74440a8c70defb0eeb4ba2c849b4e1b8bc4e
freddy01288 <freddy01288@gmail.com>
2015-03-04 12:09:04 -0500

    agregando sociales type one

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--type-one-home--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--type-one-home--block.tpl.php
@@ -3,11 +3,4 @@
     <?php print $row;?>
   <?php endforeach; ?>
 </div>
-	<div class="row">
-		<div class="small-12 columns">
-			<?php kpr($rows[0]); ?>
-			<?php dpm($rows[0]); ?>
-		</div>
-		
-	</div>
 </div>
