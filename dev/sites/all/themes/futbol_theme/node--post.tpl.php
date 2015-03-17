5cc2e8907d02881987f50b1d8c536402f9d93724
Freddy <freddy01288@gmail.com>
2015-03-17 18:47:43 -0500

    modificando create by

--- dev/sites/all/themes/futbol_theme/node--post.tpl.php
+++ dev/sites/all/themes/futbol_theme/node--post.tpl.php
@@ -113,8 +113,9 @@
           <p><?php
             $user_fields = user_load($node->uid);
             $firstname = $user_fields->field_firstname['und']['0']['value'];
-            setlocale(LC_TIME,"es_ES");
-            echo "Publicado: " . date( "F j, Y",$node->created) . " "; 
+            setlocale(LC_TIME, 'es_ES.UTF-8');
+            // setlocale(LC_TIME,"es_ES");
+            echo "Publicado: " . strftime("%A, %d de %B de %Y", $node->created) . " "; 
             echo " Autor: " . $firstname; 
           ?></p>
       <?php             
