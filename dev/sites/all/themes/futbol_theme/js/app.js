29d7d1095877af440f6cd2cf6bfa9fa1a7a97cd1
carlos calderon <cadermino@gmail.com>
2015-03-21 13:27:28 -0500

    infinite scroll

--- dev/sites/all/themes/futbol_theme/js/app.js
+++ dev/sites/all/themes/futbol_theme/js/app.js
@@ -41,3 +41,28 @@ $(document).foundation({
 
 
 });
+
+(function ($) {
+  Drupal.behaviors.viewsInfiniteScroll = {
+    attach: function (context, settings) {
+      $(function(){
+
+
+        var $container = $('div.container-type-one');
+
+
+        $container.imagesLoaded( function(){
+          $container.infinitescroll({
+            navSelector  : 'div.autopager .pager',    // selector for the paged navigation
+            nextSelector : 'div.autopager .pager-next a',  // selector for the NEXT link (to page 2)
+            itemSelector : 'div.autopager .views-row',     // selector for all items you'll retrieve
+            loading: {
+              finishedMsg: 'No more pages to load.',
+              img: Drupal.settings.basePath + 'sites/all/themes/futbol_theme/images/loading.gif'
+            }
+          })
+        })
+      });
+    }
+  };
+})(jQuery);
