6e3ae95ef73314dca6d2e59b82e06f98af5a7a3c
Freddy <freddy01288@gmail.com>
2015-03-23 10:25:13 -0500

    ordenando news sponsored

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--news-sponsored--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--news-sponsored--block.tpl.php
@@ -1,16 +1,16 @@
 
-    <?php $count = 0; ?>
-    <?php foreach ($rows as $id => $row): ?>
-			<?php if (fmod($count, 3) == 0) : ?>
-			  <div class='row'>
-			<?php endif; ?>  
+    <?php //$count = 0; ?>
+    <?php //foreach ($rows as $id => $row): ?>
+			<?php //if (fmod($count, 3) == 0) : ?>
+			  <!-- <div class='row'> -->
+			<?php //endif; ?>  
 
-<?php //foreach ($rows as $id => $row): ?>
+<?php foreach ($rows as $id => $row): ?>
   <?php print $row;?>
-<?php //endforeach; ?>
+<?php endforeach; ?>
 
-	    <?php if (fmod($count, 3) == 2) : ?>  
-		    </div>
-			<?php endif; ?> 
-			<?php $count++; ?>
-    <?php endforeach; ?>
+	    <?php //if (fmod($count, 3) == 2) : ?>  
+		    <!-- </div> -->
+			<?php //endif; ?> 
+			<?php //$count++; ?>
+    <?php //endforeach; ?>
