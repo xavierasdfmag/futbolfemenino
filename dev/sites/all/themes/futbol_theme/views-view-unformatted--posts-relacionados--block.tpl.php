26da4654ab157e92c5aad32c2bd4ef7615801225
carlos calderon <cadermino@gmail.com>
2015-04-14 01:42:43 -0500

    views relacionadas

--- dev/sites/all/themes/futbol_theme/views-view-unformatted--posts-relacionados--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-unformatted--posts-relacionados--block.tpl.php
@@ -1,6 +1,15 @@
 <div class="content">
   <?php foreach ($rows as $id => $row): ?>
     <?php print $row;?>
+    <?php  
+      function futbol_theme_views_pre_view(&$view) {
+        if ($view->name == 'posts_relacionados_hijos') {
+          kpr($view->display['default']->handler->options['filters']);
+          $view->display['default']->handler->options['filters']['province']['value'] = $tag;
+        }
+      }
+      echo views_embed_view('posts_relacionados_hijos', $display_id = 'default');
+      ?>
   <?php endforeach; ?>
 <!-- </div>
 </div> -->
