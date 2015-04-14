23dcc6a49a69b1df832ff345ea4c63a71fb6de02
carlos calderon <cadermino@gmail.com>
2015-04-14 03:07:02 -0500

    views relacionadas

--- dev/sites/all/themes/futbol_theme/views-view-fields--posts-relacionados--block.tpl.php
+++ dev/sites/all/themes/futbol_theme/views-view-fields--posts-relacionados--block.tpl.php
@@ -37,7 +37,7 @@
         print('hola');
         $view = views_get_view('posts_relacionados_hijos');
         // print_r($view);
-        $view->display_handler->display->display_options['filters']['tag']['default_value'] = $tag;
+        $view->display_handler->display->display_options['filters']['tag']['tag'] = $tag;
         $view->execute();
         print $view->render();
 
