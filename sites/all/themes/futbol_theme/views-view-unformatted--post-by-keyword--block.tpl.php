ff6dd6af6ba33977c632d30de6368d7619f6d087
Diego Chinga Olivera <diego.chinga@qualitri.com>
2015-07-01 11:02:26 -0500

    form buscar

--- sites/all/themes/futbol_theme/views-view-unformatted--post-by-keyword--block.tpl.php
+++ sites/all/themes/futbol_theme/views-view-unformatted--post-by-keyword--block.tpl.php
@@ -1,5 +1,10 @@
 <div class="row">
   <div class="small-12 large-8 columns sec-left">
+  	<?php if(!empty($rows)): ?>
 		<?php foreach ($rows as $id => $row): ?>
-		  <?php print $row;?>
+		  <?php print $row; ?>
 		<?php endforeach; ?>
+	<?php else: ?>
+		<?php print $empty; ?>
+	<?php endif; ?>
+
